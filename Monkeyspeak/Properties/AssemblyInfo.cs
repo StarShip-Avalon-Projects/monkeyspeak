c428b2bf1de0e9749ef2d2df3240c9b0513aef12
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-22 15:50:02 -0700

    updating MS from squizzles master

--- Monkeyspeak/Properties/AssemblyInfo.cs
+++ Monkeyspeak/Properties/AssemblyInfo.cs
@@ -1,10 +1,4 @@
 ﻿
-
-
-
-
-
-
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
@@ -20,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Scripting language that is very user friendly.")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Monkeyspeak")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,9 +23,11 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bedba5f0-e0cf-4ec2-b4bc-bcddcdaaca42")]
 
-[assembly: AssemblyVersion("7.0.02.5000")]
-[assembly: AssemblyFileVersion("7.0.02.5000")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("Monkeyspeak.snk")]
 [assembly: AssemblyKeyName("")]
+[assembly: NeutralResourcesLanguage("en")]
+
