de4e606323926cddb91afcc8cf662dcb1585d6f0
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-24 11:30:52 -0700

    version fix

--- Monkeyspeak/Properties/AssemblyInfo.cs
+++ Monkeyspeak/Properties/AssemblyInfo.cs
@@ -1,4 +1,10 @@
 ﻿
+
+
+
+
+
+
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
@@ -14,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Scripting language that is very user friendly.")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Monkeyspeak")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,11 +29,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("36caff91-3743-468a-bdde-a2305feefccf")]
 
+//[assembly: AssemblyVersion("1.0.02.5421")]
+//[assembly: AssemblyFileVersion("1.0.02.5421")]
+
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("Monkeyspeak.snk")]
 [assembly: AssemblyKeyName("")]
-[assembly: NeutralResourcesLanguage("en")]
-
