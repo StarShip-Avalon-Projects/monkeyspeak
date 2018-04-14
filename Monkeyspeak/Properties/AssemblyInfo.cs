b203fc6ce86107fd1c3d9687716929dfed617ba9
Jeremy Trombley <gerolkae@hotmail.com>
2018-04-14 14:30:16 -0400

    pft

--- Monkeyspeak/Properties/AssemblyInfo.cs
+++ Monkeyspeak/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("36caff91-3743-468a-bdde-a2305feefccf")]
 
-[assembly: AssemblyVersion("1.0.04.9910")]
-[assembly: AssemblyFileVersion("1.0.04.9910")]
+[assembly: AssemblyVersion("1.0.04.10412")]
+[assembly: AssemblyFileVersion("1.0.04.10412")]
 
 //[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("1.0")]
